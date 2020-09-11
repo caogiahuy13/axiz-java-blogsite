ea81e0898a8f4bbddf9b8c0aad0748a864d04393
Cao Gia Huy <caogiahuy13@gmail.com>
2020-09-11 22:41:04 +0900

    Update Screen Name

--- src/main/java/web/util/ScreenName.java
+++ src/main/java/web/util/ScreenName.java
@@ -1,12 +1,22 @@
 package web.util;
 
 public final class ScreenName {
+	public static final String TOP = "top";
 	public static final String LOGIN = "login";
 	public static final String REGISTER = "register";
+	public static final String MY_PAGE = "myPage";
+	public static final String MEMBER_INFO = "memberInfo";
+	public static final String DELETE_MEMBER = "deleteMember";
+	public static final String ARTICLE = "article";
+	public static final String CREATE_ARTICLE = "createArticle";
+	public static final String UPDATE_ARTICLE = "updateArticle";
+	public static final String DELETE_ARTICLE = "deleteArticle";
+	public static final String MY_ARTICLES = "myArticles";
+	public static final String ANALYTICS = "analytics";
+	public static final String NOTIFICATION = "notification";
+
 	public static final String MENU = "menu";
 	public static final String SEARCH = "search";
-	public static final String CREATE_ARTICLE = "createArticle";
-	public static final String ARTICLE = "article";
 	public static final String EDIT_ARTICLE = "editArticle";
 	public static final String ARTICLE_REACTIONS = "articleReactions";
 	public static final String FAVORITES = "favorites";
